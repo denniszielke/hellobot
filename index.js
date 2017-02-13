4a4fe0bd5361324441392ccd3991eeca7db48c78
Dennis Zielke <dzielke@microsoft.com>
2017-02-13 06:56:07 -0800

    Initial

---
+++ index.js
@@ -0,0 +1,67 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+var appInsights = require('applicationinsights');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD,
+    instrumentationKey: process.env.INSTRUMENTATIONKEY
+});
+
+appInsights.setup(connector.instrumentationKey).setAutoCollectRequests(true).start();
+var insightsClient = appInsights.getClient(connector.instrumentationKey);
+insightsClient.trackEvent('Initializing');
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        insightsClient.trackEvent('Received name ' + results.response);
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
